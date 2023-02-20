1973acf5fe03ec003f08acad9940954206246f1f
Sellyna <sellynalky@gmail.com>
2023-02-20 19:23:52 +1300

    icons added and margin adjusted according to screen size

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,17 +1,43 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedin,
+  faGithub,
+  faGit,
+} from "@fortawesome/free-brands-svg-icons";
+
 function Intro() {
   return (
-    <section className="flex flex-wrap flex-col mx-36">
+    <section className="flex flex-wrap flex-col 2xl:mx-36 md:mx-5">
       <h1 className="font-lato font-bold text-5xl text-title">
         I'm Sellyna Lee
       </h1>
       <p className="font-lato text-lg text-highlight mt-[10px]">
         Front End Developer / Junior Developer
       </p>
-      <p className="font-lato text-lg text-white mt-[15px] pr-36">
+      <p className="font-lato text-lg text-white mt-[15px] 2xl:pr-48 md:pr-5">
         I am a junior developer based in the beautiful city of Auckland, New
         Zealand. I like to build code, bring interface designs to life and
         learning new technologies in the digital space.
       </p>
+
+      <div className="mt-6">
+        <a href="https://www.linkedin.com/in/sellyna-lee/">
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            size="xl"
+            inverse
+            className="pr-[20px]"
+          />
+        </a>
+        <a href="https://github.com/SewinaCode">
+          <FontAwesomeIcon
+            icon={faGithub}
+            size="xl"
+            inverse
+            className="pl-[20px]"
+          />
+        </a>
+      </div>
     </section>
   );
 }
